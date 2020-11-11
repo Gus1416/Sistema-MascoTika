9764d328a9986cfa1be6110994ce8d61b3b84803
Sebastian <Sebastian@DESKTOP-PHP5KCJ>
2020-11-10 21:19:24 -0600

    Parte 1 del tutorial

--- src/Modelo/ProcedimientoDAO.java
+++ src/Modelo/ProcedimientoDAO.java
@@ -13,31 +13,9 @@ import java.util.List;
         
         
 public class ProcedimientoDAO {
-   Conexion conectar = new Conexion();
-   Connection con;
-   PreparedStatement ps;
-   ResultSet rs;
-   
-   public List listar () {
-       List<Procedimiento> datos = new ArrayList<> ();
-       String sql = "select * from Procedimiento";
-       try {
-           con = Conexion.conectar();
-           ps = con.prepareStatement(sql);
-           rs = ps.executeQuery();
-           while (rs.next()){
-               Procedimiento p = new Procedimiento();
-               p.setIDProcedimiento(rs.getInt(1));
-                p.setTipo(rs.getString(2));
-                p.setMontoUnitario(rs.getInt(3));
-                p.setMontoServAdom(rs.getInt(4));
-                datos.add(p);
-            }
-        } catch (Exception e) {
-        }
-        return datos;
     
-   }
+    public Procedimiento insertar(int ID,String tipo,int MontoUnitario)
+    
+
+
 }
-
-
